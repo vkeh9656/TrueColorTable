0d760c7b40176ca0ca2c8a76c483967ec1fddd9f
sjpark <tjdwns9656@naver.com>
2023-01-18 16:36:20 +0900

    512x512 사이즈로 1차 완성 후 확대본 적용, 확대본 첫번째 사각형에 mouse 이벤트 추가

--- TrueColorTableDlg.cpp
+++ TrueColorTableDlg.cpp
@@ -18,7 +18,7 @@
 
 
 CTrueColorTableDlg::CTrueColorTableDlg(CWnd* pParent /*=nullptr*/)
-	: CDialogEx(IDD_TRUECOLORTABLE_DIALOG, pParent)
+	: CDialogEx(IDD_TRUECOLORTABLE_DIALOG, pParent), m_view_rect(0, 0, 160, 128)
 {
 	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
 }
@@ -31,6 +31,10 @@ void CTrueColorTableDlg::DoDataExchange(CDataExchange* pDX)
 BEGIN_MESSAGE_MAP(CTrueColorTableDlg, CDialogEx)
 	ON_WM_PAINT()
 	ON_WM_QUERYDRAGICON()
+	ON_WM_LBUTTONDOWN()
+	ON_WM_LBUTTONUP()
+	ON_WM_MOUSEMOVE()
+	ON_WM_DESTROY()
 END_MESSAGE_MAP()
 
 
@@ -45,7 +49,59 @@ BOOL CTrueColorTableDlg::OnInitDialog()
 	SetIcon(m_hIcon, TRUE);			// 큰 아이콘을 설정합니다.
 	SetIcon(m_hIcon, FALSE);		// 작은 아이콘을 설정합니다.
 
-	// TODO: 여기에 추가 초기화 작업을 추가합니다.
+	m_color_image.Create(4096, 4096, 32);
+	m_draw_image.Create(1920, 1080, 32);
+	m_pick_image.Create(16, 16, 32);
+
+	m_draw_dc.Attach(m_draw_image.GetDC());
+
+	
+	BITMAP bmp_info;
+	GetObject((HBITMAP)m_color_image, sizeof(BITMAP), &bmp_info);
+	mp_color_pattern = (unsigned int*)m_color_image.GetBits();
+	mp_color_pattern += bmp_info.bmWidthBytes / sizeof(unsigned int) - 1;
+
+	
+	GetObject((HBITMAP)m_pick_image, sizeof(BITMAP), &bmp_info);
+	mp_pick_pattern = (unsigned int*)m_pick_image.GetBits();
+	mp_pick_pattern += bmp_info.bmWidthBytes / sizeof(unsigned int) - 1;
+
+	unsigned int* p = mp_color_pattern;
+	int x = 0, y = 0, step = 0;
+	for (int i = 0; i < 4096 * 4096; i++)
+	{
+		*(p - y * 4096 - x - step * 256) = i;
+		x = (x + 1) % 256;
+		if (!x) y++;
+		if (y >= 4096) 
+		{
+			y = 0;
+			step++;
+		}
+		/**p-- = i;*/
+	}
+
+	unsigned int* p_src = mp_color_pattern;
+	unsigned int* p_dest = mp_pick_pattern;
+
+	for (int i = 0; i < 256; i++)
+	{
+		if (i && !(i % 16)) p_src -= 4096 * 255;
+		*p_dest = *p_src;
+
+		p_src -= 256;
+		p_dest--;
+	}
+
+	m_color_image.Draw(m_draw_dc, 0, 0, 512, 512);
+	m_color_image.Draw(m_draw_dc, 512, 0, 256 * 5, 256 * 4, 0, 0, 256 * 5, 256 * 4);
+	m_pick_image.Draw(m_draw_dc, 0, 512, 512, 512);
+
+	m_draw_dc.SelectStockObject(NULL_BRUSH);
+	m_draw_dc.SelectStockObject(WHITE_PEN);
+	m_draw_dc.Rectangle(m_view_rect);
+
+	ShowWindow(SW_MAXIMIZE);
 
 	return TRUE;  // 포커스를 컨트롤에 설정하지 않으면 TRUE를 반환합니다.
 }
@@ -56,9 +112,10 @@ BOOL CTrueColorTableDlg::OnInitDialog()
 
 void CTrueColorTableDlg::OnPaint()
 {
+	CPaintDC dc(this); // 그리기를 위한 디바이스 컨텍스트입니다.
+
 	if (IsIconic())
-	{
-		CPaintDC dc(this); // 그리기를 위한 디바이스 컨텍스트입니다.
+	{	
 
 		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
 
@@ -75,7 +132,9 @@ void CTrueColorTableDlg::OnPaint()
 	}
 	else
 	{
-		CDialogEx::OnPaint();
+		m_draw_image.Draw(dc, 0, 0);
+
+		//CDialogEx::OnPaint();
 	}
 }
 
@@ -86,3 +145,105 @@ HCURSOR CTrueColorTableDlg::OnQueryDragIcon()
 	return static_cast<HCURSOR>(m_hIcon);
 }
 
+
+
+void CTrueColorTableDlg::OnLButtonDown(UINT nFlags, CPoint point)
+{
+	if (m_view_rect.PtInRect(point))
+	{
+		m_is_clicked = 1;
+		m_prev_pos = point;
+		SetCapture();
+	}
+	else if (CRect(512, 0, 512 + 256, 256).PtInRect(point))
+	{
+		m_is_clicked = 2;
+	}
+
+	CDialogEx::OnLButtonDown(nFlags, point);
+}
+
+
+void CTrueColorTableDlg::OnLButtonUp(UINT nFlags, CPoint point)
+{
+	if (m_is_clicked ==1) ReleaseCapture();
+	
+	m_is_clicked = 0;
+
+	CDialogEx::OnLButtonUp(nFlags, point);
+}
+
+
+void CTrueColorTableDlg::OnMouseMove(UINT nFlags, CPoint point)
+{
+	if (m_is_clicked == 1) 
+	{
+		CPoint pos = point - m_prev_pos;
+		m_view_rect += pos;
+		m_prev_pos = point;
+
+		if (m_view_rect.left < 0)
+		{
+			m_view_rect.right -= m_view_rect.left;
+			m_view_rect.left = 0;
+		}
+		else if (m_view_rect.right > 512)
+		{
+			m_view_rect.right = 512;
+			m_view_rect.left = m_view_rect.right - 160;
+		}
+
+		if (m_view_rect.top < 0)
+		{
+			m_view_rect.bottom -= m_view_rect.top;
+			m_view_rect.top = 0;
+		}
+		else if (m_view_rect.bottom > 512)
+		{
+			m_view_rect.bottom = 512;
+			m_view_rect.top = m_view_rect.bottom - 128;
+		}
+
+		m_color_image.Draw(m_draw_dc, 0, 0, 512, 512);
+		m_color_image.Draw(m_draw_dc, 512, 0, 256 * 5, 256 * 4, m_view_rect.left << 3, m_view_rect.top << 3, 256 * 5, 256 * 4);
+		m_pick_image.Draw(m_draw_dc, 0, 512, 512, 512);
+
+		m_draw_dc.SelectStockObject(NULL_BRUSH);
+		m_draw_dc.SelectStockObject(WHITE_PEN);
+		m_draw_dc.Rectangle(m_view_rect);
+		
+		CClientDC dc(this);
+		m_draw_image.Draw(dc, 0, 0);
+	}
+
+	else if (m_is_clicked == 2 && CRect(512, 0, 512 + 256, 256).PtInRect(point))
+	{
+		unsigned int* p_src = mp_color_pattern - point.y * 4096 
+							- (255 - (point.x - 512));
+		unsigned int* p_dest = mp_pick_pattern;
+
+		for (int i = 0; i < 256; i++)
+		{
+			if (i && !(i % 16)) p_src -= 4096 * 255;
+
+			*p_dest = *p_src;
+			p_src -= 256;
+			p_dest--;
+		}
+
+		m_pick_image.Draw(m_draw_dc, 0, 512, 512, 512);
+		CClientDC dc(this);
+		m_draw_image.Draw(dc, 0, 0);
+	}
+
+	CDialogEx::OnMouseMove(nFlags, point);
+}
+
+
+void CTrueColorTableDlg::OnDestroy()
+{
+	CDialogEx::OnDestroy();
+
+	m_draw_dc.Detach();
+	m_draw_image.ReleaseDC();
+}
--- TrueColorTableDlg.h
+++ TrueColorTableDlg.h
@@ -8,6 +8,18 @@
 // CTrueColorTableDlg 대화 상자
 class CTrueColorTableDlg : public CDialogEx
 {
+private:
+	CImage m_color_image;
+	unsigned int* mp_color_pattern, *mp_pick_pattern; // RGB를 한번에 묶어쓰기 위해 unsigned int형으로 선언
+	
+	CImage m_draw_image, m_pick_image;
+	CDC m_draw_dc;
+
+	CRect m_view_rect;
+
+	CPoint m_prev_pos;
+	unsigned char m_is_clicked = 0;
+
 // 생성입니다.
 public:
 	CTrueColorTableDlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
@@ -30,4 +42,9 @@ protected:
 	afx_msg void OnPaint();
 	afx_msg HCURSOR OnQueryDragIcon();
 	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnLButtonDown(UINT nFlags, CPoint point);
+	afx_msg void OnLButtonUp(UINT nFlags, CPoint point);
+	afx_msg void OnMouseMove(UINT nFlags, CPoint point);
+	afx_msg void OnDestroy();
 };
