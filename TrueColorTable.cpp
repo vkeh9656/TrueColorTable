7fd8b6380d06495d21cc505eb6208a704d85e8cf
sjpark <tjdwns9656@naver.com>
2023-01-18 11:16:16 +0900

    초기 커밋.

---
+++ TrueColorTable.cpp
@@ -0,0 +1,46 @@
+﻿
+// TrueColorTable.cpp: 애플리케이션에 대한 클래스 동작을 정의합니다.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "TrueColorTable.h"
+#include "TrueColorTableDlg.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CTrueColorTableApp
+
+BEGIN_MESSAGE_MAP(CTrueColorTableApp, CWinApp)
+	ON_COMMAND(ID_HELP, &CWinApp::OnHelp)
+END_MESSAGE_MAP()
+
+
+// CTrueColorTableApp 생성
+
+CTrueColorTableApp::CTrueColorTableApp()
+{
+	// TODO: 여기에 생성 코드를 추가합니다.
+	// InitInstance에 모든 중요한 초기화 작업을 배치합니다.
+}
+
+
+// 유일한 CTrueColorTableApp 개체입니다.
+
+CTrueColorTableApp theApp;
+
+
+// CTrueColorTableApp 초기화
+
+BOOL CTrueColorTableApp::InitInstance()
+{
+	CWinApp::InitInstance();
+
+	CTrueColorTableDlg dlg;
+	dlg.DoModal();
+	return FALSE;
+}
+
---
+++ TrueColorTableDlg.h
@@ -0,0 +1,33 @@
+﻿
+// TrueColorTableDlg.h: 헤더 파일
+//
+
+#pragma once
+
+
+// CTrueColorTableDlg 대화 상자
+class CTrueColorTableDlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CTrueColorTableDlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_TRUECOLORTABLE_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
